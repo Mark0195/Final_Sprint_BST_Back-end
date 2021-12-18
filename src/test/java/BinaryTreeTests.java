7a8d8e43a8260237141d2dbac9af6347ca71db3b
Mark0195 <mark.myles@keyin.com>
2021-12-18 11:42:34 -0330

    added tests

--- src/test/java/BinaryTreeTests.java
+++ src/test/java/BinaryTreeTests.java
@@ -1,7 +1,7 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
-
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNull;
+//import static org.junit.jupiter.api.Assertions.assertSame;
+//
 //import org.junit.jupiter.api.Test;
 //
 //public class BinaryTreeTests {
---
+++ src/test/java/Database/DataRest/BSTTest.java
@@ -0,0 +1,16 @@
+package Database.DataRest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BSTTest {
+    @Test
+    void testConstructor() {
+        BST actualBst = new BST();
+        actualBst.setId(123L);
+        actualBst.setInput("Input");
+        assertEquals(123L, actualBst.getId());
+        assertEquals("Input", actualBst.getInput());
+    }
+}
